003a5dc9b6401dc74f394aa1c1c5012c8aff323a
Gabriel <41166074+gckopper@users.noreply.github.com>
2024-08-16 12:37:10 -0300

    adding example cmake and fixing paths

--- cmake/AddGlad.cmake
+++ cmake/AddGlad.cmake
@@ -1,9 +1,9 @@
-if(NOT EXISTS "${PROJECT_SOURCE_DIR}/extern/glad/src/gl.c")
+if(NOT EXISTS "${PROJECT_SOURCE_DIR}/../extern/glad/src/gl.c")
     message(FATAL_ERROR "Glad not found!")
 endif()
 
 # Path to glad directory
-set(GLAD_SOURCES_DIR "${PROJECT_SOURCE_DIR}/extern/glad/")
+set(GLAD_SOURCES_DIR "${PROJECT_SOURCE_DIR}/../extern/glad/")
 
 include_directories(${GLAD_SOURCES_DIR}/include)
 file(GLOB BUTTERFLIES_SOURCES_C ${CMAKE_CURRENT_SOURCE_DIR} *.c ${GLAD_SOURCES_DIR}/src/gl.c)
